d894c2661f83f70de0e973a9408f73cc3655dade
VezRoland <vezrolaa@gmail.com>
2021-06-12 12:37:09 +0200

    Update kod.js

--- kod.js
+++ kod.js
@@ -8,14 +8,13 @@ function tortenes() {
     console.log(window.innerWidth)
     if (szel > 1920) {
         borito.style.height = "500px";
+    } else if (szel <= 1920 && szel > 695) {
+        borito.style.height = "400px";
     } else if (szel <= 695 && szel > 520) {
         borito.style.height = "300px";
     } else if (szel <= 520 && szel > 360) {
         borito.style.height = "200px";
     } else if (szel <= 360) {
         borito.style.height = "100px";
-    } else {
-        borito.style.height = "400px";
-    }   
-}
+    }
 }
