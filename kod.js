9b7e7689bac5443d5add5b16a4a466291af44c8a
VezRoland <vezrolaa@gmail.com>
2021-06-12 12:33:31 +0200

    Update kod.js

--- kod.js
+++ kod.js
@@ -1,10 +1,21 @@
 var szel = window.innerWidth;
 var borito = document.getElementById("borito");
 
-if (szel <= 695 && szel > 520) {
-    borito.style.height = "300px";
-} else if (szel <= 520 && szel > 360) {
-    borito.style.height = "200px";
-} else if (szel <= 360) {
-    borito.style.height = "100px";
+window.addEventListener("resize", tortenes);
+
+function tortenes() {
+    szel = window.innerWidth;
+    console.log(window.innerWidth)
+    if (szel > 1920) {
+        borito.style.height = "500px";
+    } else if (szel <= 695 && szel > 520) {
+        borito.style.height = "300px";
+    } else if (szel <= 520 && szel > 360) {
+        borito.style.height = "200px";
+    } else if (szel <= 360) {
+        borito.style.height = "100px";
+    } else {
+        borito.style.height = "400px";
+    }   
+}
 }
