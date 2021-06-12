33cf3f7f9f905c12662885788c2e7744786c4dc6
VezRoland <vezrolaa@gmail.com>
2021-06-12 10:01:17 +0200

    Create kod.js

---
+++ kod.js
@@ -0,0 +1,14 @@
+var szel = window.innerWidth;
+var borito = document.getElementById("borito");
+
+window.addEventListener("resize", allito);
+
+function allito() {
+    if (szel <= 695 && szel > 520) {
+        borito.style.height = "300px";
+    } else if (szel <= 520 && szel > 360) {
+        borito.style.height = "200px";
+    } else if (szel <= 360) {
+        borito.style.height = "100px";
+    }    
+}
